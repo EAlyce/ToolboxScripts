ac42231ebb9546acb33571a2cb0c3aecbcf100c4
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-08-18 14:26:53 +0800

    Create disable_ipv6.sh

---
+++ disable_ipv6.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p

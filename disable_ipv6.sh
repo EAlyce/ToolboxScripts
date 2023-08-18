86b38d718f2fd7fd47eebdb829dc5fc3849a64b2
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-08-18 14:30:57 +0800

    Update disable_ipv6.sh

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -3,3 +3,6 @@ echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
+echo "IPv4 和 IPv6 的当前状态："
+ip -4 addr show
+ip -6 addr show
